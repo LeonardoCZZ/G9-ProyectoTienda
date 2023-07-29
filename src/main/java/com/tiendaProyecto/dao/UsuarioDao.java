14c42be87594d3a585e0741a3fd1f82283650444
andrewbocker <andrew.bocker26@gmail.com>
2023-07-28 19:46:16 -0700

    actualizacion login y seguridad

---
+++ src/main/java/com/tiendaProyecto/dao/UsuarioDao.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.tiendaProyecto.dao;
+
+import com.tiendaProyecto.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author andre
+ */
+public interface UsuarioDao extends JpaRepository<Usuario, Long>{
+    
+     Usuario findByUsername(String username);
+
+}
