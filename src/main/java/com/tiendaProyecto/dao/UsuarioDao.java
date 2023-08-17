ba023da299e9c6ee2b4f3d06b38e8f91758cce74
andrewbocker <andrew.bocker26@gmail.com>
2023-08-17 12:13:16 -0700

    corregir errores

--- src/main/java/com/tiendaProyecto/dao/UsuarioDao.java
+++ src/main/java/com/tiendaProyecto/dao/UsuarioDao.java
@@ -13,7 +13,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface UsuarioDao extends JpaRepository<Usuario, Long>{
     
-     Usuario findByUsername(String username);
+    Usuario findByUsername(String username);
 
     Usuario findByUsernameAndPassword(String username, String Password);
 
